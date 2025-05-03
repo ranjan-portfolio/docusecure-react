018f1e6c19d230cc8243c14fcebfae83fb845e64
ranjan <ranjan@example.com>
2025-05-03 23:09:07 +0100

    code refactor

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    __AUTHORITY__: JSON.stringify("https://cognito-idp.eu-west-2.amazonaws.com/eu-west-2_EGZ4NHv3x"),
+    __CLIENT_ID__: JSON.stringify("3r5u33672k7gvmvdg2b6fclq2j"),
+    __REDIRECT_URI__: JSON.stringify("http://localhost:5173"),
+  }
+  
 })
