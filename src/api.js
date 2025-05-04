b187c98b124e2c9426bbe9e901af5050a4548c27
ranjan <ranjan@example.com>
2025-05-04 06:51:00 +0100

    refactoring

--- src/api.js
+++ src/api.js
@@ -1,18 +1,16 @@
 import axios from "axios";
 
-const API_BASE_URL=import.meta.env.VITE_API_BASE_URL;
 
-console.log("API_BASE_URL ", API_BASE_URL);
+
+console.log( __API_BASE_URL__);
 
 const api=axios.create({
-    baseURL:API_BASE_URL,
+    baseURL:__API_BASE_URL__,
 })
 
 export const setAuthToken=(token)=>{
     console.log("setAuthToken is called");
     if(token){
-       // console.log("token is ",token);
-        //console.log("API_BASE_URL ", API_BASE_URL);
         api.defaults.headers.common['Authorization']=`Bearer ${token}`;
     }else{
         delete api.defaults.headers.common['Authorization'];
--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
     __AUTHORITY__: JSON.stringify("https://cognito-idp.eu-west-2.amazonaws.com/eu-west-2_EGZ4NHv3x"),
     __CLIENT_ID__: JSON.stringify("3r5u33672k7gvmvdg2b6fclq2j"),
     __REDIRECT_URI__: JSON.stringify("http://localhost:5173"),
+    __API_BASE_URL__: JSON.stringify("http://localhost:8080/api")
   }
   
 })
